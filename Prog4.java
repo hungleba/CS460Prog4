3728142709008a95e2cd8539990b027008c50788
Hung Le Ba <hungleba@cs.arizona.edu>
2022-12-05 21:01:47 -0700

    Initial commit

---
+++ Prog4.java
@@ -0,0 +1,371 @@
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.time.Duration;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
+import java.util.Scanner;
+public class Program4 {
+    /*---------------------------------------------------------------------
+        |  Method endProgram(Connection dbconn, Scanner inputReader)
+        |
+        |  Purpose: Making final stabilization step to make sure
+        |       the program ends without any memory loss.
+        |
+        |  Pre-condition:  None
+        |
+        |  Post-condition: Print out the end line for program UI, disconnect
+        |       from the Oracle database, and close STDOUT reader.
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void endProgram(Connection dbconn, Scanner inputReader) {
+        System.out.println("----------------End of the program----------------");
+        inputReader.close();
+        try {
+            dbconn.close();
+        } catch (SQLException e) {
+            System.out.println("ERR: Cannot close database connection!");
+        }
+        System.exit(0);
+    }
+
+    /*---------------------------------------------------------------------
+        |  Method promptLogin(String[] args)
+        |
+        |  Purpose: Get account information from the user and 
+        |       connect to Oracle database
+        |
+        |  Pre-condition: given account and password must be valid
+        |
+        |  Post-condition: None
+        |
+        |  Parameters:
+        |      args -- account username and password passed via command-line arguments
+        |
+        |  Returns: a Connection object used to connect to Oracle database
+    *-------------------------------------------------------------------*/
+    private static Connection promptLogin(String[] args) {
+        final String oracleURL =   // Magic lectura -> aloe access spell
+                        "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+
+        String username = null,    // Oracle DBMS username
+               password = null;    // Oracle DBMS password
+
+        if (args.length == 2) {    // get username/password from cmd line args
+            username = args[0];
+            password = args[1];
+        } else {
+            System.out.println("\nUsage:  java Prog3 <username> <password>\n"
+                             + "    where <username> is your Oracle DBMS"
+                             + " username,\n    and <password> is your Oracle"
+                             + " password (not your system password).\n");
+            System.exit(-1);
+        }
+
+            // load the (Oracle) JDBC driver by initializing its base
+            // class, 'oracle.jdbc.OracleDriver'.
+
+        try {
+                Class.forName("oracle.jdbc.OracleDriver");
+        } catch (ClassNotFoundException e) {
+                System.err.println("*** ClassNotFoundException:  "
+                    + "Error loading Oracle JDBC driver.  \n"
+                    + "\tPerhaps the driver is not on the Classpath?");
+                System.exit(-1);
+        }
+        
+            // make and return a database connection to the user's
+            // Oracle database
+
+        Connection dbconn = null;
+        try {
+                dbconn = DriverManager.getConnection
+                               (oracleURL,username,password);
+        } catch (SQLException e) {
+                System.err.println("*** SQLException:  "
+                    + "Could not open JDBC connection.");
+                System.err.println("\tMessage:   " + e.getMessage());
+                System.err.println("\tSQLState:  " + e.getSQLState());
+                System.err.println("\tErrorCode: " + e.getErrorCode());
+                System.exit(-1);
+        }
+        return dbconn;
+    }
+
+    private static void recordInsertion(Connection dbconn, Scanner inputReader) {
+        String input = "";
+        Integer userInput = 0;
+        while (userInput != -1) {
+            System.out.println("Which data would you like to add? (Enter -1 to exit):");
+            System.out.println("    1. Customer");
+            System.out.println("    2. Flight");
+            System.out.println("    3. Employee");
+            System.out.println("    4. Customer's Flight History");
+            input = inputReader.nextLine().trim();
+            try {
+                userInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println("ERR: please enter an integer");
+                userInput = 0;
+                continue;
+            }
+            if (userInput == -1) {
+                break;
+            }
+            if (userInput == 1) {
+                recordInsertionCustomer(dbconn, inputReader);
+            } else if (userInput == 2) {
+                recordInsertionFlight(dbconn, inputReader);
+            } else if (userInput == 3) {
+                recordInsertionEmployee(dbconn, inputReader);
+            } else if (userInput == 4) {
+                recordInsertionCustomerFlightHistory(dbconn, inputReader);
+            }
+        }
+    }
+
+    private static void recordDeletion(Connection dbconn, Scanner inputReader) {
+        String input = "";
+        Integer userInput = 0;
+        while (userInput != -1) {
+            System.out.println("Which data would you like to delete? (Enter -1 to exit):");
+            System.out.println("    1. Customer");
+            System.out.println("    2. Flight");
+            System.out.println("    3. Customer's Flight History");
+            input = inputReader.nextLine().trim();
+            try {
+                userInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println("ERR: please enter an integer");
+                userInput = 0;
+                continue;
+            }
+            if (userInput == -1) {
+                break;
+            }
+            if (userInput == 1) {
+                recordDeletionCustomer(dbconn, inputReader);
+            } else if (userInput == 2) {
+                recordDeletionFlight(dbconn, inputReader, null);
+            } else if (userInput == 3) {
+                recordDeletionStaff(dbconn, inputReader);
+            }
+        }
+    }
+
+    private static void recordUpdate(Connection dbconn, Scanner inputReader) {
+        // TO-DO
+    }
+
+    /*---------------------------------------------------------------------
+        |  Method performQuery(Connection dbconn, Scanner inputReader)
+        |
+        |  Purpose: elet the user choose from 5 queries to perform 
+        |
+        |  Pre-condition:  tables inclduing data for the query already exist 
+        |                   in the database. 
+        |
+        |  Post-condition: 
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void performQuery(Connection dbconn, Scanner inputReader) {
+        System.out.println("----------------QUERY----------------");
+        System.out.println("Please choose from one of the following queries to perform:");
+        System.out.println("1. Display list of distinct passenger names who flew all 4 airlines in 2021.");
+        System.out.println("2. For input airlines and a date in Mar 2021, display list of passenger and their checked bag count.");
+        System.out.println("3. For input date in June 2021, display schedules of fligth in ascending order of boarding time.");
+        System.out.println("4. For three categories (Student, Frequent Flyer and Handicap) of United Airlines, display passengers who:");
+        System.out.println("\ta. Traveled only once in the month of March.");
+        System.out.println("\tb. Traveled with exactly one checked in bag anytime in the months of June and July.");
+        System.out.println("\tc. Ordered snacks/beverages on at least on one flight.");
+        System.out.println("5. For an input airline, display all the total number of passengers in each category that flew in 2021.");
+        Integer userInput = 0;
+        while (userInput != -1){
+            String input = inputReader.nextLine().trim();
+            try {
+                userInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println("ERR: please enter an integer");
+                userInput = 0;
+                continue;
+            }
+            if (userInput == 1) queryOne(dbconn);
+            if (userInput == 1) queryTwo(dbconn, inputReader);
+            if (userInput == 1) queryThree(dbconn, inputReader);
+            if (userInput == 1) queryFour(dbconn);
+            if (userInput == 1) queryFive(dbconn, inputReader);
+        }
+    }
+
+    /*---------------------------------------------------------------------
+        |  Method queryFive(Connection dbconn, Scanner inputReader)
+        |
+        |  Purpose: ask user for a specific airlines and print out count of 
+        |           distinct passengers of each categories who flew that airlines in 2021 
+        |
+        |  Pre-condition:  tables inclduing data for the query already exist 
+        |                   in the database. 
+        |
+        |  Post-condition: N/A
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void queryFive(Connection dbconn, Scanner inputReader) {
+    }
+    /*---------------------------------------------------------------------
+        |  Method queryFour(Connection dbconn, Scanner inputReader)
+        |
+        |  Purpose: For 3 categories of the passengers of United Airlines,
+        |   a.   Traveled only once in the month of March
+        |   b.   Traveled with exactly one checked in bag anytime in the months of June and July.
+        |   c.   Ordered snacks/beverages on at least on one flight.
+        |
+        |  Pre-condition:  tables inclduing data for the query already exist 
+        |                   in the database. 
+        |
+        |  Post-condition: N/A
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void queryFour(Connection dbconn) {
+    }
+
+    /*---------------------------------------------------------------------
+        |  Method queryThree(Connection dbconn, Scanner inputReader)
+        |
+        |  Purpose: For input date in June 2021, display schedules of flight
+        |           in ascending order of boarding time.
+        |  Pre-condition:  tables inclduing data for the query already exist 
+        |                   in the database. 
+        |
+        |  Post-condition: N/A
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void queryThree(Connection dbconn, Scanner inputReader) {
+    }
+
+    /*---------------------------------------------------------------------
+        |  Method queryTwo(Connection dbconn, Scanner inputReader)
+        |
+        |  Purpose: For input date in June 2021, display schedules of flight
+        |           in ascending order of boarding time.
+        |  Pre-condition:  tables inclduing data for the query already exist 
+        |                   in the database. 
+        |
+        |  Post-condition: N/A
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void queryTwo(Connection dbconn, Scanner inputReader) {
+    }
+
+    /*---------------------------------------------------------------------
+        |  Method queryOne(Connection dbconn)
+        |
+        |  Purpose: Display list of distinct passenger names who 
+        |           flew all 4 airlines in 2021.
+        |  Pre-condition:  tables inclduing data for the query already exist 
+        |                   in the database. 
+        |
+        |  Post-condition: N/A
+        |
+        |  Parameters:
+        |      dbconn -- a Connection object, use for Oracle database connection
+        |      inputReader -- a Java Scanner object, use to read input from users
+        |
+        |  Returns:  None.
+    *-------------------------------------------------------------------*/
+    private static void queryOne(Connection dbconn) {
+        String cusID_query = "SELECT DISTINCT CusID FROM CUSTOMER";
+        Statement cusID_stmt = null;
+        ResultSet cusID_result = null;
+        String current_cusID = null;
+        String flight_count_query = "SELECT COUNT(DISTINCT AirlineID) " +
+                                    "FROM HISTORY" + current_cusID+ "FROM CUSTOMER";
+        Statement cusID_stmt = null;
+        ResultSet cusID_result = null;
+        try {
+            stmt = dbconn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                answer.next();
+                employeeId = answer.getInt("NEXTVAL");
+            }
+            stmt.close();  
+        } catch (SQLException e) {
+                System.err.println("*** SQLException:  "
+                    + "Could not get unique ID.");
+                System.err.println("\tMessage:   " + e.getMessage());
+                System.err.println("\tSQLState:  " + e.getSQLState());
+                System.err.println("\tErrorCode: " + e.getErrorCode());
+                System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Connecting to Oracle DB through JDBC.
+        Connection dbconn = promptLogin(args);
+        Scanner inputReader = new Scanner(System.in);
+        String input = "";
+        Integer userInput = 0;
+        // Frontend interface/menu
+        System.out.println("----------------CS460 Program 4----------------");
+        while (userInput != -1) {
+            System.out.println("-----------------------------------------------");
+            System.out.println("Main menu:");
+            System.out.println("    1. Insert");
+            System.out.println("    2. Delete");
+            System.out.println("    3. Update");
+            System.out.println("    4. Perform Query");
+            System.out.print("Which operation would you like to perform? (Enter -1 to exit): ");
+            input = inputReader.nextLine().trim();
+            try {
+                userInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println("ERR: please enter an integer");
+                userInput = 0;
+                continue;
+            }
+            if (userInput == -1) {
+                break;
+            }
+            if (userInput == 1) {
+                recordInsertion(dbconn, inputReader);
+            } else if (userInput == 2) {
+                recordDeletion(dbconn, inputReader);
+            } else if (userInput == 3) {
+                recordUpdate(dbconn, inputReader);
+            } else {
+                performQuery(dbconn, inputReader);
+            }
+        }
+        endProgram(dbconn, inputReader);
+    }
+}
---
+++ create_table.sql
@@ -0,0 +1,68 @@
+-- Create CUSTOMER table & sequence
+CREATE TABLE CUSTOMER (
+    CusId INT NOT NULL,
+    Name VARCHAR2(50) NOT NULL,
+    DOB DATE,
+    Address VARCHAR2(100),
+    FrequentFlyer INT NOT NULL,
+    Student INT NOT NULL,
+    Handicap INT NOT NULL,
+    PRIMARY KEY (CusId)
+);
+CREATE SEQUENCE CUSTOMER_SEQ START WITH 1 INCREMENT BY 1;
+
+-- Create AIRLINES table & sequence
+CREATE TABLE AIRLINES (
+    AirlineId INT NOT NULL,
+    Name VARCHAR2(10),
+    PRIMARY KEY (AirlineId)
+);
+INSERT INTO AIRLINES (AirlineId, Name) VALUES (1, 'Delta');
+INSERT INTO AIRLINES (AirlineId, Name) VALUES (2, 'SouthWest');
+INSERT INTO AIRLINES (AirlineId, Name) VALUES (3, 'United');
+INSERT INTO AIRLINES (AirlineId, Name) VALUES (4, 'Alaska');
+
+-- Create EMPLOYEE table & sequence
+CREATE TABLE EMPLOYEE (
+    EmployeeId INT NOT NULL,
+    AirlineId INT NOT NULL,
+    Name VARCHAR2(50),
+    DOB DATE,
+    Role VARCHAR2(12),
+    PRIMARY KEY (EmployeeId),
+    FOREIGN KEY (AirlineId) REFERENCES AIRLINES (AirlineId)
+);
+CREATE SEQUENCE EMPLOYEE_SEQ START WITH 1 INCREMENT BY 1;
+
+-- Create FLIGHT table & sequence
+CREATE TABLE FLIGHT (
+    FlightId INT NOT NULL,
+    AirlineId INT NOT NULL,
+    PilotId INT NOT NULL,
+    CrewId INT NOT NULL,
+    GroundStaffId INT NOT NULL,
+    BoardGate INT NOT NULL,
+    BoardTime DATE NOT NULL,
+    DepartTime DATE NOT NULL,
+    Duration INTERVAL DAY TO SECOND NOT NULL,
+    DepAirport VARCHAR2(3) NOT NULL,
+    ArrAirport VARCHAR2(3) NOT NULL,
+    PRIMARY KEY (FlightId),
+    FOREIGN KEY (AirlineId) REFERENCES AIRLINES (AirlineId),
+    FOREIGN KEY (PilotId) REFERENCES EMPLOYEE (EmployeeId),
+    FOREIGN KEY (CrewId) REFERENCES EMPLOYEE (EmployeeId),
+    FOREIGN KEY (GroundStaffId) REFERENCES EMPLOYEE (EmployeeId)
+);
+CREATE SEQUENCE FLIGHT_SEQ START WITH 1 INCREMENT BY 1;
+
+
+-- Create HISTORY table
+CREATE TABLE HISTORY (
+    CusId INT NOT NULL,
+    FlightId INT NOT NULL,
+    LuggageCount INT NOT NULL,
+    FoodOrderCount INT NOT NULL,
+    PRIMARY KEY (CusId, FlightId),
+    FOREIGN KEY (CusId) REFERENCES CUSTOMER (CusId),
+    FOREIGN KEY (FlightId) REFERENCES FLIGHT (FlightId)
+);
